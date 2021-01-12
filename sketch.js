77dca8066d21e75ce10fc206152c65acded7e2fa
babuji8 <73380497+babuji8@users.noreply.github.com>
2021-01-12 21:07:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+
+
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1200, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	paper1=new paper(200,575);
+	var rightBox1=createSprite(1025,550,20,70);
+	var leftBox2=createSprite(885,550,20,70);
+	var bottomBox3=createSprite(955,575,120,20);
+	rightBox1.shapeColor="red"
+	leftBox2.shapeColor="red"
+	bottomBox3.shapeColor="red"
+
+	ground1=new ground(600,595,1200,20);
+
+
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+  Engine.update(engine);
+  paper1.display();
+  ground1.display();
+  
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:15,y:-15});
+	}
+}
+
+
+
