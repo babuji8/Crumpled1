5c45c365fc5a31c8fb93a6f61dd4872ee596d729
babuji8 <73380497+babuji8@users.noreply.github.com>
2021-01-14 21:10:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 
-
+var rightBox1,leftBox2,bottomBox3,paper1,ground1
 
 
 
@@ -20,21 +19,24 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
-	paper1=new paper(200,575);
-	var rightBox1=createSprite(1025,550,20,70);
-	var leftBox2=createSprite(885,550,20,70);
-	var bottomBox3=createSprite(955,575,120,20);
-	rightBox1.shapeColor="red"
-	leftBox2.shapeColor="red"
-	bottomBox3.shapeColor="red"
+	
+	
 
-	ground1=new ground(600,595,1200,20);
+	
 
+	ground1 = new ground(600,595,1200,20);
 
-
+	
+	
 	//Create the Bodies Here.
-
-
+	rightBox1 = createSprite(1025,550,20,70);
+	leftBox2 = createSprite(885,550,20,70);
+	bottomBox3 = createSprite(955,575,120,20);
+   
+	rightBox1.shapeColor = "red";
+   leftBox2.shapeColor = "red";
+   bottomBox3.shapeColor = "red";
+   paper1=new paper(200,575);
 	Engine.run(engine);
   
 }
@@ -42,20 +44,22 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(255);
+  background("Blue");
   Engine.update(engine);
-  paper1.display();
+ 
   ground1.display();
   
+  paper1.display();
   drawSprites();
  
 }
 
 function keyPressed(){
 	if(keyCode===UP_ARROW){
-		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:15,y:-15});
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:50,y:-50});
 	}
 }
+
 
 
 
